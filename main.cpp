99d5b6cb91b90f077927233f15d8d90d547d3c56
池田大樹 <65146226+ikedataiki@users.noreply.github.com>
2020-05-15 16:32:47 +0900

    main.cpp
    Add fairuuuuu

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include"DxLib.h"
+#include"common.h"
+
+#include "GameTask.h"
+///テスト
+
+
+
+//------------メイン処理
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	
+
+	GameTask*gameTask;//ゲームタスク管理用変数
+	gameTask = new GameTask();
+
+	//------メインループ
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) != -1)
+	{
+		ClsDrawScreen();
+		
+		gameTask->GameMainLoop();
+		
+		ScreenFlip();
+	}
+	DxLib_End; //DxLibの後始末
+	return 0; //ソフトの終了
+}
+
